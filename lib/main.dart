6c48bc1c3d2334310f3fd9e0ca34aaf724b2f67f
Alif Haikal <alifhaikal.1992@gmail.com>
2020-05-18 12:02:35 +0800

    update UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello!'),);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App2'),
+        ),
+        body: Text('Some Text2'),
+      ),
+    );
   }
 }
