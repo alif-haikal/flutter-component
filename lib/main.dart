4c7198c912c0577771a2002785b5827051ceb7e4
Alif Haikal <alifhaikal.1992@gmail.com>
2020-05-18 14:09:56 +0800

    convert stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;   
+      print('this Answer Chosen! $questionIndex');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+      'What\'s your favourite movie?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App2'),
         ),
-        body: Text('Some Text2'),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('answer 2 choosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //... some logic code
+                print('answer 3 chosen');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
